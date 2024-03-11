6daeaff23654b392ec4051063a0d7c2753afb49e
kiran-farhan <159946236+kiran-farhan@users.noreply.github.com>
2024-03-11 08:07:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// // data type(string)
+ let myname: string=("kiran farhan");
+ console.log (myname);
+// // data type(number)
+ let age: number = 43;
+ console.log(age);
+// // data type (boolean)
+ let isit: boolean = true;
+ console.log (isit)
+// // datatype (any)
+ let value: any = "goole.com" ;
+ console.log (value);
+// // datatype (undefined)
+  let a;
+  console.log(a);
+// //  datatype(null)
+ let abc:any = 
+ console.log ("abc");
+// // camelcase
+ let myComputer :'generation seven';
+ console.log ('myComputer');
+// // snakecase
+ let my_computer :string =("it is not easy for me")
+ console.log( "my_computer");
+// // pascalcase
+ let MyComputer :any = "10 ,'year old'";
+ console.log(MyComputer)
+// array []
+var classstudent = ["ali", "ammar", "farhan", "sumair"];
+console.log(classstudent);
+console.log(classstudent[0], classstudent[3]);
